8a8665bc71f39782072d9f1694e375d85f342144
Simon Ibssa <ibssasimon@gmail.com>
2020-01-11 20:51:55 -0800

    adding image files for blending

--- Programs/SIMON_IBSSA_PROGRAM_1/imageblend.c
+++ Programs/SIMON_IBSSA_PROGRAM_1/imageblend.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
-int main() {
-  printf("Hello!\n");
+typedef unsigned short WORD;
+typedef unsigned int DWORD;
+typedef unsigned int LONG;
+struct tagBITMAPFILEHEADER {
+  WORD bfType; //specifies the file type
+  DWORD bfSize; //specifies the size in bytes of the bitmap file
+  WORD bfReserved1; //reserved; must be 0
+  WORD bfReserved2; //reserved; must be 0
+  DWORD bfOffBits; //species the offset in bytes from the bitmapfileheader to the bitmap bits
+};
+
+struct tagBITMAPINFOHEADER {
+  DWORD biSize; //specifies the number of bytes required by the struct
+  LONG biWidth; //specifies width in pixels
+  LONG biHeight; //species height in pixels
+  WORD biPlanes; //specifies the number of color planes, must be 1
+  WORD biBitCount; //specifies the number of bit per pixel
+  DWORD biCompression;//spcifies the type of compression
+  DWORD biSizeImage; //size of image in bytes
+  LONG biXPelsPerMeter; //number of pixels per meter in x axis
+  LONG biYPelsPerMeter; //number of pixels per meter in y axis
+  DWORD biClrUsed; //number of colors used by th ebitmap
+  DWORD biClrImportant; //number of colors that are important
+};
+
+
+int main(int argc, char *argv[]) {
+  printf("%d\n", argc);
+  
   return 0;
 }
