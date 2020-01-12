19bbc9a1bcabedef37c43e0d383c73ed3cc04f4d
Simon Ibssa <ibssasimon@gmail.com>
2020-01-11 20:15:13 -0800

    just need to add helper functions

--- lab2/SIMONIBSSA_list.c
+++ lab2/SIMONIBSSA_list.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
+// function declarations
+int getInput();
+
+// Defining listelement struct
+typedef struct listelement listelement;
+ struct listelement {
+  listelement *next, *prev;
+  char text[1000];
+};
 int main() {
-  printf("Hello\n");
+  //Initializing double linked list
+
+
+  int userData;
+  while((userData = getInput()) != 4) {
+    if(userData == 1) {
+      printf("I'm going to push a string\n");
+    } else if(userData == 2) {
+      printf("I'm going to print a list\n");
+    } else if(userData == 3) {
+      printf("I'm going to delete an item\n");
+    } else if(userData < 0 || userData > 4) {
+      return 0;
+    }
+  }
   return 0;
+}
+
+int getInput() {
+  // Function gets user input and determines next step of program
+  int input;
+  printf("Select:\n");
+  printf("1 push string\n");
+  printf("2 print list\n");
+  printf("3 delete item\n");
+  printf("4 end program\n");
+  scanf("%d", &input);
+  return input;
 }
