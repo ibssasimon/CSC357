d2e9f250a51e6c886d7e14851d1a3cd3e075e994
Simon Ibssa <ibssasimon@gmail.com>
2020-01-22 15:07:36 -0800

    adding helper functions and pseudo code

--- lab3/SIMONIBSSA_dma.c
+++ lab3/SIMONIBSSA_dma.c
@@ -1,35 +1,78 @@
 #include <stdio.h>
 #include <string.h>
 
-// global array declaration - size of 1MB
-unsigned char myheap[1048576];
-
 // pagesize definition
 #define PAGESIZE 1024
+#define _1MB 1048576 
+
+
+// global array declaration - size of 1MB
+unsigned char myheap[_1MB];
+
+typedef unsigned char BYTE;
 
 typedef struct chunkhead {
   unsigned int size;
   unsigned int info;
-  unsigned char* next;
-  unsigned char* prev;
+  BYTE* next;
+  BYTE* prev;
 
 }chunkhead;
 
 // function declarations
-unsigned char* ibssaloc(unsigned int size);
-void myfree(unsigned char* myaddres);
+BYTE* ibssaloc(unsigned int size);
+void myfree(BYTE* myaddres);
+void merge(BYTE* chunk1, BYTE* chunk2);
+BYTE* split(BYTE* chunk);
 
 
 int main() {
   printf("my dynamic memory allocation assignment\n");
+
+  // defining first chunkhead
+  chunkhead *ch = (chunkhead*)myheap;
+  ch -> info = 0;
+  ch -> size = _1MB - sizeof(chunkhead);
+  ch -> next = 0;
+  ch -> prev = 0;
+
+
   return 0;
 }
 
-unsigned char* ibssaloc(unsigned int size) {
-  return NULL;
+
+
+BYTE* ibssaloc(unsigned int size) {
+  // get first chunkhead, typecasted
+  chunkhead* ch = (chunkhead*)myheap;
+
+  // check ch-> size with size
+  // check ch -> info with 0 or 1
+      // if false, go to next chunkhead
+
+      // if true, set ch to new values
+      // split chunkhead into mem value
+      // link chunks
+      // set parameters
+      // return ((BYTE*)chunkhead) + 16;
+
+  
+
+  // iterate through all chunkheads checking size and info
+
+  return 0;
 }
 
 
-void myfree(unsigned char* myaddress) {
+void myfree(BYTE* myaddress) {
+  // implementation of free function
 
+}
+
+void merge(BYTE* chunk1, BYTE* chunk2) {
+  // implentation of merge function
+}
+
+BYTE* split(BYTE* chunk) {
+  // implementation of split function. Should return address of new chunk.
 }
