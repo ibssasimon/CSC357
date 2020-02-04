6f07513bc3f6e07548be30a1789f826a8043b2d6
Simon Ibssa <ibssasimon@gmail.com>
2020-02-03 17:16:00 -0800

    yay christian

--- lab5/SIMONIBSSA_forkit_lab5/forkit_1.c
+++ lab5/SIMONIBSSA_forkit_lab5/forkit_1.c
@@ -8,12 +8,19 @@ int main() {
   fflush(0);
   if(fork() == 0) {
     // child process 1st fork
-    printf("1\n");
+
+    if(fork() == 0) {
+      // child 2nd fork
+      printf("1\n");
+      return 1;
+    } else {
+      wait(&g);
+      printf("2\n");
+    }
     return 1;
   } else {
     // parent process 1st fork
     wait(&g);
-    printf("2\n");
     if(fork() == 0) {
       // child 2nd fork
       printf("3\n");
---
+++ midterm/midterm.c
@@ -0,0 +1,14 @@
+/* Simon Ibssa, CPE 357-03*
+   Christian Eckhardt
+   Midterm 1
+*/
+#include <stdio.h>
+
+int main() {
+  int i = 0;
+
+  for(i = 0; i < 9; i++) {
+    printf("%d", i);
+  }
+  return 0;
+}
