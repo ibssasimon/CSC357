f3cf285af19d79f67a55d70b9f0fc0badbabcc52
Simon Ibssa <ibssasimon@gmail.com>
2020-02-10 20:12:24 -0800

    adding child changes

--- SIMONIBSSA_signals_lab6/simonsignal.c
+++ SIMONIBSSA_signals_lab6/simonsignal.c
@@ -2,17 +2,25 @@
 #include <unistd.h>
 #include <time.h>
 int main() {
+  printf("Nearly indestructable child program! Muahahahaha!\n");
   printf("Main program ID: %d\n", getpid());
-  printf("---BEGIN FORKING----\n");
-  fflush(0);
   int g;
+  fflush(0);
+
+  
   if(fork() == 0) {
     // child process
-    printf("I am the child, my ID is: %d\n", getpid());
+    printf("child ID: %d\n", getpid());
+    while(1) {
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    printf("the time is %02d:%02d:%02d\n", tm.tm_hour % 12, tm.tm_min, tm.tm_sec);
+    sleep(5);
+    }
     return 1;
   } else {
     wait(&g);
-    printf("I am the parent, my ID is: %d\n", getpid());
+    printf("parent ID: %d\n", getpid());
   }
   return 0;
 }
