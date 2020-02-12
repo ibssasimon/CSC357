d52333ab807ed57e1c876bfe54b1926ad1a12b68
Simon Ibssa <ibssasimon@gmail.com>
2020-02-11 20:18:20 -0800

    so i have an indestructable program

--- SIMONIBSSA_signals_lab6/simonsignal.c
+++ SIMONIBSSA_signals_lab6/simonsignal.c
@@ -1,26 +1,61 @@
+// Simon Ibssa, CSC 357
+
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/mman.h>
+#include <signal.h>
+
+
+// function declarations
+void overwriteSignal(int n);
+
 int main() {
+
+  // overwriting signals
+  signal(SIGINT, overwriteSignal);
+  signal(SIGTSTP, overwriteSignal);
+
+
+
   printf("Nearly indestructable child program! Muahahahaha!\n");
   printf("Main program ID: %d\n", getpid());
   int g;
   fflush(0);
 
-  
   if(fork() == 0) {
+    fflush(0);
     // child process
     printf("child ID: %d\n", getpid());
     while(1) {
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-    printf("the time is %02d:%02d:%02d\n", tm.tm_hour % 12, tm.tm_min, tm.tm_sec);
-    sleep(5);
+      time_t t = time(NULL);
+      struct tm tm = *localtime(&t);
+      printf("the time is %02d:%02d:%02d\n", tm.tm_hour % 12, tm.tm_min, tm.tm_sec);
+      sleep(5);
     }
     return 1;
   } else {
-    wait(&g);
+    if(wait(&g) != NULL) {
+      if(fork() == 0) {
+        fflush(0);
+        while(1) {
+          time_t t = time(NULL);
+          struct tm tm = *localtime(&t);
+          printf("the time is %02d:%02d:%02d\n", tm.tm_hour % 12, tm.tm_min, tm.tm_sec);
+          sleep(5);
+        }
+        return 1;
+      } else {
+        wait(&g);
+      }
+    }
+
     printf("parent ID: %d\n", getpid());
   }
   return 0;
+}
+
+// helper functions
+void overwriteSignal(int n) {
+  printf("NOPE\n");
 }
