b19f46a2e5ccf9c135577b78dc7e016bc7b79dd0
Simon Ibssa <ibssasimon@gmail.com>
2020-02-13 20:32:42 -0800

    some test files

--- SIMONIBSSA_signals_ass3/monitor1.c
+++ SIMONIBSSA_signals_ass3/monitor1.c
@@ -7,12 +7,31 @@
 
 struct stat st;
 int main() {
+  int g;
+  printf("\033[0;34m"); // set output color to blue
+  printf("stat prog");
+  printf("\033[0m"); //Resets the text to default color
+  printf(" $\n");
+
 
   if(fork() == 0) {
     // child process
-    
+    int result = stat("test.txt", &st);
+
+    if(result == -1) {
+      printf("Could not open file!\n");
+    }
+
+    printf("%d\n", st.st_mode);
+    printf("%d\n", st.st_size);
+
+
+
+    return 1;
   } else {
     // parent process
+    wait(&g);
+
   }
   return 0;
 }
