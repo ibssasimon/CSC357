54cecc68070dd1ba5798b3d697377614f79b59e8
Simon Ibssa <ibssasimon@gmail.com>
2020-02-14 16:31:38 -0800

    stuff

--- SIMONIBSSA_signals_ass3/monitor1.c
+++ SIMONIBSSA_signals_ass3/monitor1.c
@@ -4,9 +4,13 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
+#include <dirent.h>
 
 
 struct stat st;
+struct dirent* dent;
+DIR* dir;
+
 int main() {
   int g;
   //printf("\033[0;34m"); // set output color to blue
@@ -14,28 +18,41 @@ int main() {
   //printf("\033[0m"); //Resets the text to default color
   //printf(" $\n");
 
-
-
+  int* time = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+  *time = 0;
   if(fork() == 0) {
     // child process
+    
     char file[100];
     char userInput[100];
-    
+    char buffer[50];
+    fflush(0);
 
+    
+    clock_t start = clock();
     while(1) {
       printf("Enter an option: [filename] [list] or [q]: ");
       scanf("%s", userInput);
 
       // listing content of current directory
       if(strcmp(userInput, "list") == 0) {
-        printf("Listing content of current directory!\n");
+        strcpy(buffer, ".");
+        dir = opendir(buffer);
+
+        if(dir != NULL) {
+          for(dent = readdir(dir); dent != NULL; dent = readdir(dir)) {
+            printf("%s", dent -> d_name);
+
+            if(dent -> d_type == DT_DIR) {
+              printf(" - is a directory");
+            }
+            printf("\n");
+          }
+        }
       } else if(strcmp(userInput, "q") == 0) {
         return 1;
       } else {
         // assume it's a file name
-        printf("\n");
-
-
         int result = stat(userInput, &st);
 
         if(result == -1) {
@@ -61,13 +78,20 @@ int main() {
 
     }
 
-
+    clock_t end = clock();
+    int childTime = ((double)(end - start)) / CLOCKS_PER_SEC;
+    *time = childTime;
     return 1;
   } else {
     // parent process
+    fflush(0);
+    if(*time > 10) {
+      printf("loser! Hurry up ya dammy!\n");
+    }
     wait(&g);
+    printf("child has finished!\n");
 
   }
+
   return 0;
-  printf("%s", ctime(&st.st_mtimespec));
 }
