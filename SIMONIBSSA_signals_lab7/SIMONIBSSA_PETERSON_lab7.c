feee37d2ca871912ba7e5b9eea931d364fcb51ff
Simon Ibssa <ibssasimon@gmail.com>
2020-02-20 11:38:58 -0800

    thanks christian

--- SIMONIBSSA_signals_lab7/SIMONIBSSA_PETERSON_lab7.c
+++ SIMONIBSSA_signals_lab7/SIMONIBSSA_PETERSON_lab7.c
@@ -11,40 +11,53 @@ int main() {
 
   // variables for peterson's algo
   int* pr = (int*)mmap(NULL, 2, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1 , 0);
-  int* flag = (int*)mmap(NULL, 1000, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1 , 0);
+  int* flag = (int*)mmap(NULL, 1, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1 , 0);
+  char* text = (char*)mmap(NULL, 1000, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
   pr[0] = 0;
   pr[1] = 0;
   *flag = 0;
 
+  char sentence1[] = {"Aye! Aye! Aye! Grrrraaah!! Ge ge ge grraaah!! Panda, Panda Panda, Panda, Panda, Panda, Panda."};
+  char sentence2[] = {"I got dogs in Atlanta, twisting rope, clean, and the Fanta (soda). Brah brah brah"};
+  char outString[1000];
+  int i;
   // fork into child and parent processes
   if(fork() == 0) {
     // child case
-    char sentence1[] = "Aye! Aye! Aye! Grrrraaah!! Ge ge ge grraaah!! Panda, Panda Panda, Panda, Panda, Panda, Panda.";
-    char sentence2[] = "I got dogs in Atlanta, twisting rope, clean, and the Fanta (soda).";
-    pr[0] = 1;
-    *flag = 1;
-    // critical section
-    while(pr[1] == 1 && *flag == 1);
+   
 
-    // infinite while loop
-    while(1) {
-      // check if i % 2 == 0 , if yes copy sent1 into sentences, else copy sent2 into sentesnces.
+    for(i = 0;;i=!i) {
+      *flag = 1;
+      pr[0] = 1;
+      while(pr[1] == 1 && *flag == 1);
+
+      if(i % 2 == 0) {
+        // critical section
+        strcpy(text, sentence1);
+      } else {
+        strcpy(text, sentence2);
+      }
+      // ending critical section
+      pr[0] = 0;
     }
+   
     
-    pr[0] = 0;
 
   } else {
     // parent case
-    char myString[1000];
-    pr[1] = 1;
-    *flag = 0;
-    //critical section
-    while(pr[0] == 1 && *flag == 0);
-    strcpy(myString, sentences);
-    printf("%s", myString);
-    pr[1] = 0;
-
+    
+    while(1) {
+      pr[1] = 1;
+      *flag = 0;
+      while(pr[0] == 1 && *flag == 0);
+      //critical section
+      strcpy(outString, text);
+      printf("%s\n", outString);
+      // ending critical section
+      pr[1] = 0;
+    }
+  
   }
   return 0;
 }
