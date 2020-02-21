e4f893000d24784151381d5bbb29e7c71d89d46c
Simon Ibssa <ibssasimon@gmail.com>
2020-02-20 21:24:38 -0800

    adding some code for dekker

--- SIMONIBSSA_signals_lab7/SIMONIBSSA_DEKKER_lab7.c
+++ SIMONIBSSA_signals_lab7/SIMONIBSSA_DEKKER_lab7.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <string.h>
+#include <stdbool.h>
 
 /* Simon Ibssa, CSC 357. Lab 7 using PETERSON ALGO*/
 
@@ -10,13 +11,13 @@ int main() {
 
 
   // variables for peterson's algo
-  int* pr = (int*)mmap(NULL, 2, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1 , 0);
-  int* flag = (int*)mmap(NULL, 1, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1 , 0);
+  bool* wantToEnter = (bool*)mmap(NULL, 2, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1 , 0);
+  int* turn = (int*)mmap(NULL, 1, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1 , 0);
   char* text = (char*)mmap(NULL, 1000, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
-  pr[0] = 0;
-  pr[1] = 0;
-  *flag = 0;
+  wantToEnter[0] = false;
+  wantToEnter[1] = false;
+  *turn = 0;
 
   char sentence1[] = {"Aye! Aye! Aye! Grrrraaah!! Ge ge ge grraaah!! Panda, Panda Panda, Panda, Panda, Panda, Panda."};
   char sentence2[] = {"I got dogs in Atlanta, twisting rope, clean, and the Fanta (soda). Brah brah brah"};
@@ -26,11 +27,12 @@ int main() {
   if(fork() == 0) {
     // child case
    
-
+    /* TODO(sibssa): implement p0 for dekker */
+    
     for(i = 0;;i=!i) {
-      *flag = 1;
-      pr[0] = 1;
-      while(pr[1] == 1 && *flag == 1);
+      *turn = 1;
+      wantToEnter[0] = 1;
+      while(wantToEnter[1] == 1 && *turn == 1);
 
       if(i % 2 == 0) {
         // critical section
@@ -39,7 +41,7 @@ int main() {
         strcpy(text, sentence2);
       }
       // ending critical section
-      pr[0] = 0;
+      wantToEnter[0] = 0;
     }
    
     
@@ -48,14 +50,14 @@ int main() {
     // parent case
     
     while(1) {
-      pr[1] = 1;
-      *flag = 0;
-      while(pr[0] == 1 && *flag == 0);
+      wantToEnter[1] = 1;
+      *turn = 0;
+      while(wantToEnter[0] == 1 && *turn == 0);
       //critical section
       strcpy(outString, text);
       printf("%s\n", outString);
       // ending critical section
-      pr[1] = 0;
+      wantToEnter[1] = 0;
     }
   
   }
