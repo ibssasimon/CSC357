ca6e93761c979d0b2885d7af643575aa1dc06322
Simon Ibssa <ibssasimon@gmail.com>
2020-02-27 14:02:55 -0800

    adding logic for asgn4

--- SIMONIBSSA_findstuff_ass4/findstuff.c
+++ SIMONIBSSA_findstuff_ass4/findstuff.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
   printf("find stuff program\n");
+
+
+  if(fork() == 0) {
+    // child case
+    /* 
+      CHILD should search, report and end
+    */
+  } else {
+    // parent case
+
+    /*
+      Parent should do the scanf. COPY ASGN 3 SCANF CODE HERE
+     */
+  }
   return 0;
 }
