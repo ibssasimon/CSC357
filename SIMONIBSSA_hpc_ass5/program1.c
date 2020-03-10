2c825110169b877cdc61b69feaf910b4abebaa02
Simon Ibssa <ibssasimon@gmail.com>
2020-03-10 16:08:22 -0700

    just need to correctly implement matrix mult

--- SIMONIBSSA_hpc_ass5/program1.c
+++ SIMONIBSSA_hpc_ass5/program1.c
@@ -93,7 +93,7 @@ void quadratic_matrix_multiplication_parallel(float *A,float *B,float *C, int pa
           C[a + b*MATRIX_DIMENSION_XY] = 0.0;
   //multiply
   for(int a = 0;a<MATRIX_DIMENSION_XY;a++) // over all cols a
-      for(int b = start;b<end;b++) // over all rows b
+      for(int b = 0;b<MATRIX_DIMENSION_XY;b++) // over all rows b
           for(int c = 0;c<MATRIX_DIMENSION_XY;c++) // over all rows/cols left
               {
                   C[a + b*MATRIX_DIMENSION_XY] += A[c + b*MATRIX_DIMENSION_XY] * B[a + c*MATRIX_DIMENSION_XY]; 
